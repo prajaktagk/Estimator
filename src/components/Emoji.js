576d41d820dd138c7d12fe339e70f4f68222fa3e
prajaktagk <abhangprajakta1@gmail.com>
2023-06-18 15:17:31 +0530

    My 1st Estimator Project

---
+++ src/components/Emoji.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EmojiPicker from 'emoji-picker-react';
+
+function Emoji() {
+  return (
+    <div>
+      <EmojiPicker />
+    </div>
+  );
+}
+
+export default Emoji;
